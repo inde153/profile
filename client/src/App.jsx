41f4244a4159a52bfe0548fc2a1b42ed0baa81e9
inde153 <inde153@naver.com>
2023-01-04 23:55:28 +0900

    Add : theme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,14 @@
 import styled, { ThemeProvider } from "styled-components";
 import React, { Component } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import { darkTheme, lightTheme } from "./styles/theme.ts";
-import GlobalStyle from "./styles/GlobalStyle.ts";
+import GlobalStyle from "./styles/GlobalStyle.tsx";
 import RandingPage from "./page/RandingPage.tsx";
 import Header from "./components/Header.tsx";
 import Footer from "./components/Footer.tsx";
 
 const App = () => {
-  const [isDark, setIsDark] = useState(false);
+  const [isDark, setIsDark] = useState(true);
 
   return (
     <>
